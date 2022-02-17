632f69938d55f2aa9823e8e704cade421ba61b71
Ryan Viers <ryanviersiv@hotmail.com>
2022-02-17 13:16:10 -0500

    Update /scripts.js file to pokedex
    Upgrade file /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-alert('Hello world');//added alert() funtion
-
-let favoriteFood='crab legs';//added variable favoriteFood
-document.write(favoriteFood);
-
+let pokemonList = [
+	{name: 'Bulbasaur', height: 0.7, type: ['grass','poison']},
+  {name: 'Charmander', height: 0.6, type: 'fire'},
+  {name: 'Squirtle', height: 0.5, type: 'water'}
+];
