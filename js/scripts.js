979a8ca3d0c0af577303a45a5be194176f1ccc59
Ryan Viers <ryanviersiv@hotmail.com>
2022-02-27 15:41:21 -0500

    Update scripts.js
    Remove old code and comments.

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,6 @@ let pokemonRepository = (function () {
   pokemonList array. If the pokemon passed to function clears validation it
   is added to the array.*/ 
   function add(pokemon){
-
-    //let obj1 = Object.keys(pokemon);
-    //let obj2 = Object.keys(pokemonList[0]);
-
-    /*Function that checks validation for pokemon added to pokemonList array. If 
-    variables obj1 and obj2 have the same number of keys in each array and the characters
-    of both arrays match when passed into strings, function returns true, otherwise the
-    function returns false.*/ 
-    /*function isEqual(x, y){//validates user entry for pokemon
-
-      if (x.length === y.length && x.toString() === y.toString()){//compares number of keys
-        return true;
-      }else {
-        console.log('false');
-        alert('You have entered Invalid Information');
-        return false;
-      }
-    }*/
- 
     if (typeof pokemon === 'object' && "name" in pokemon){
       console.log('true');
       pokemonList.push(pokemon);
@@ -39,7 +20,7 @@ let pokemonRepository = (function () {
   between the object key name and the user entry. Function will return array
   with object that matches user input, otherwise it returns an empty array.*/
   function pokeTest (var1){
-    return pokemonList.filter((nameCheck) => nameCheck.name.toLowerCase() === var1.toLowerCase())
+    return pokemonList.filter((nameCheck) => nameCheck.name.toLowerCase() === var1.toLowerCase());
   }
 
   /*Funtion that returns pokemonList*/ 
@@ -110,22 +91,12 @@ let pokemonRepository = (function () {
   };
 })();
 
-//let item = {name: 'Pikachu', height: 1.05, type: 'electric'};
-//pokemonRepository.add(item);
-
-//let userInput = prompt("What pokemon are you searching for?");
-//let result = pokemonRepository.pokeTest(userInput);
-
 pokemonRepository.loadList().then(function (){
   pokemonRepository.getAll().forEach(function (pokemon) {
       pokemonRepository.addListItem(pokemon);
   });
 })
-/*if (result.length !== 0){
-  document.write(`<br>Here is your Pokemon: <br><br> ${result[0].name} (height: ${result[0].height})`);
-}else {
-  document.write(`<br><br>${userInput} is an Invalid Entry!!!`);
-}*/
+
 
 
 
