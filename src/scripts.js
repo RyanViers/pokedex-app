0278bfb95eadbcac39b5dac2abc391b3a1ff3f93
RyanViers <ryanviersiv@hotmail.com>
2022-03-17 15:00:17 -0400

    Update scripts.js
    Linted using esLint for changing double quotes to single, and removed unnecessary code.

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,6 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
-  let dialogPromiseReject;
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   searchPokemon();
 
   /*Function loads API data for each pokemon object into pokemonList array.*/
@@ -28,12 +27,12 @@ let pokemonRepository = (function () {
   pokemonList array. If the pokemon passed to function clears validation it
   is added to the array.*/
   function add(pokemon) {
-    if (typeof pokemon === "object" && "name" in pokemon) {
+    if (typeof pokemon === 'object' && 'name' in pokemon) {
       pokemonList.push(pokemon);
     } else {
       console.log(pokemon);
-      alert("You have entered Invalid Information");
-      console.log("error");
+      alert('You have entered Invalid Information');
+      console.log('error');
     }
   }
 
@@ -45,17 +44,17 @@ let pokemonRepository = (function () {
   /*Function adds button for list item of each pokemon object, and sends the
   button and the pokemon object to the addClick() function.*/
   function addListItem(pokemon) {
-    let button = document.createElement("button");
+    let button = document.createElement('button');
     button.innerText = pokemon.name;
-    button.classList.add("btn", "btn-primary", "button-class");
+    button.classList.add('btn', 'btn-primary', 'button-class');
 
     /* Adds the data toggle and data target to trigger the modal.*/
-    button.setAttribute("data-toggle", "modal");
-    button.setAttribute("data-target", "#modal-container");
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#modal-container');
 
-    document.querySelector("#pokeList").appendChild(button);
+    document.querySelector('#pokeList').appendChild(button);
 
-    button.addEventListener("click", () => {
+    button.addEventListener('click', () => {
       showDetails(pokemon);
     });
   }
@@ -96,9 +95,9 @@ let pokemonRepository = (function () {
 
   /*Function creates modal for each individual pokemon when user clicks on individual pokemon button.*/
   function showModal(pokemon) {
-    let modalTitle = $(".modal-title");
-    let modalBody = $(".modal-body");
-    let modalFooter = $(".modal-footer");
+    let modalTitle = $('.modal-title');
+    let modalBody = $('.modal-body');
+    let modalFooter = $('.modal-footer');
 
     modalTitle.empty();
     modalBody.empty();
@@ -107,10 +106,10 @@ let pokemonRepository = (function () {
     let nameElement = $(`<h1>${pokemon.name}</h1>`);
 
     let imageElementFront = $('<img class="modal-image" style="width:50%">');
-    imageElementFront.attr("src", pokemon.imageUrlFront);
+    imageElementFront.attr('src', pokemon.imageUrlFront);
 
     let imageElementBack = $('<img class="modal-image" style="width:50%">');
-    imageElementBack.attr("src", pokemon.imageUrlBack);
+    imageElementBack.attr('src', pokemon.imageUrlBack);
 
     let heightElement = $(`<p><br>Height: ${pokemon.height}</p><br>`);
 
@@ -132,13 +131,13 @@ let pokemonRepository = (function () {
   /*Function that puts a search button in the h1 element that
   opens the showDialog() modal when pressed.*/
   function searchPokemon() {
-    let searchButton = document.querySelector("#searchButton");
+    let searchButton = document.querySelector('#searchButton');
 
     /* Adds the data toggle and data target to trigger the modal.*/
-    searchButton.setAttribute("data-toggle", "modal");
-    searchButton.setAttribute("data-target", "#modal-container");
+    searchButton.setAttribute('data-toggle', 'modal');
+    searchButton.setAttribute('data-target', '#modal-container');
 
-    searchButton.addEventListener("click", () => {
+    searchButton.addEventListener('click', () => {
       showDialog();
     });
   }
@@ -146,15 +145,15 @@ let pokemonRepository = (function () {
   /*Function that shows a input dialog that ask for the pokemon
   name that user is searching for.*/
   function showDialog() {
-    let modalTitle = $(".modal-title");
-    let modalBody = $(".modal-body");
-    let modalFooter = $(".modal-footer");
+    let modalTitle = $('.modal-title');
+    let modalBody = $('.modal-body');
+    let modalFooter = $('.modal-footer');
 
     modalTitle.empty();
     modalBody.empty();
     modalFooter.empty();
 
-    let searchQuestion = $(`<h1>Type Pokemon Name In Search Bar: </h1>`);
+    let searchQuestion = $('<h1>Type Pokemon Name In Search Bar: </h1>');
 
     let userInput = $('<input type="text" class="form-control">');
 
@@ -167,18 +166,17 @@ let pokemonRepository = (function () {
     modalFooter.append(confirmButton);
     confirmButton.focus(); //Focus the confirmButton so that the user can simply press Enter.
 
-    return new Promise((resolve, reject) => {
-      confirmButton.on("click", () => {
+    return new Promise((resolve) => {
+      confirmButton.on('click', () => {
         let pokeCheck = pokeTest(userInput.val());
         if (pokeCheck.length === 0) {
-          console.log("error");
-          alert("You Have Entered Invalid Information!");
+          console.log('error');
+          alert('You Have Entered Invalid Information!');
         } else {
           showDetails(pokeCheck[0]);
         }
         resolve();
       });
-      dialogPromiseReject = reject;
     });
   }
 
@@ -192,13 +190,13 @@ let pokemonRepository = (function () {
   }
 
   return {
-    add: add,
-    getAll: getAll,
-    pokeTest: pokeTest,
-    addListItem: addListItem,
-    loadList: loadList,
-    loadDetails: loadDetails,
-    searchPokemon: searchPokemon,
+    add,
+    getAll,
+    pokeTest,
+    addListItem,
+    loadList,
+    loadDetails,
+    searchPokemon,
   };
 })();
 
